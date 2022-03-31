d6feb135d0a8238afc401bc5721502f80cc50925
BenderRodriguez <experienceanalytics.sdk@acoustic.co>
2022-03-30 22:30:37 -0700

    Beta Tealeaf build: 10.6.162
    Beta Tealeaf Change Notes:
    Bug Fixes:
    
    - UIC queued data for WKWebview was being sent back in wrong order. Made an adjust to javascript script.
    - Update syntax due to swift issue with NSNumber during json creation.
    - Killswitch returns sometimes too slow and page displayed is not captured.
    - Gestures should not be captures on Webviews.
    
    Improvements:
    
    - New minimal os is 12.0 from 9.0 for library.
    - Correct name of page and get class name versus just getting UINavigationController.
    
    Our environment for this release:
    
    - Xcode 13.2.1
    - MacOS 11.6.1
    - iOS 12.x to 15.x
    - Supported architectures:
    	- simulator
    		- arm64
    		- x86_64
    	- device
    		- arm64
    
    Known Issues:
    
    - Captures gesture even when the gestureRecognizer is set to return 'NO', this is an Apple defect.
    - After iOS 13, library no longer supports iPad because of Apple changes in iPad OS.
    - The pink boarder on Gestures screenview is off when captured using ER, because if timing or garbage collection.
    - Template issue - The back link is missing during capture on some screens Progressindicator, AlertViewController, Gestures screenview and picker view.
    - Template issue - Battery icon not visible in green anymore.
    - Improvement :- 'connectionType' remains N/A if device is on WiFi on app start.
    - DoPostOnScreenChange is not working as expected, because of temp queue.
    - Reporting of multiple carrier names in JSON if the device has Dual SIM is a beta feature.
    - React Native has an issue for dynamic builds. We will use static version instead.
    - Certain Alerts may appear correctly during replay.
    - If a View Controller has more than one WKWebViews, gestures may be off by a few pixels
    - iPadOS Multi Window apps are not YET supported.

--- SDKs/iOS/DebugTealeafReactNative/TealeafReactNative.xcframework/ios-arm64_x86_64-simulator/TealeafReactNative.framework/Headers/TLFApplicationHelper.h
+++ SDKs/iOS/DebugTealeafReactNative/TealeafReactNative.xcframework/ios-arm64_x86_64-simulator/TealeafReactNative.framework/Headers/TLFApplicationHelper.h
@@ -1,5 +1,5 @@
 //
-// Copyright (C) 2021 Acoustic, L.P. All rights reserved.
+// Copyright (C) 2022 Acoustic, L.P. All rights reserved.
 //
 // NOTICE: This file contains material that is confidential and proprietary to
 // Acoustic, L.P. and/or other developers. No license is granted under any intellectual or
@@ -9,7 +9,15 @@
 //
 #import <Foundation/Foundation.h>
 #import <WebKit/WebKit.h>
+
+#if TEALEAF_USE_REACT
 #import <TealeafReactNative/TLFPublicDefinitions.h>
+#else
+#import <TealeafReactNative/TLFPublicDefinitions.h>
+#endif
+
+
+//#import <TealeafReactNative/TLFPublicDefinitions.h>
 
 @interface TLFApplicationHelper : NSObject <TLFLibDelegate, TLFApplicationHelperJSProtocol>
 
